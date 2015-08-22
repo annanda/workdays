f3be71d91aae57dead95b67b84f693bb735ab6bf
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-21 23:26:23 -0300

    criando o model Workday e alterando coisas na view new

--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -1,4 +1,17 @@
 class WorkdaysController < ApplicationController
     def new
     end
+
+    def create
+        @workday = Workday.new(workday_params)
+
+        @workday.save
+        redirect_to @workday
+    end
+
+    end
+    private
+    def workday_params
+        params.require(:workday).permit(:day, :arrivaltime, :lunch, :lunchleave, :lunchback, :departuretime)
+    end
 end
