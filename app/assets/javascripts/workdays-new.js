675040d7748ad5bf24de6eb2c3e602698022fc7a
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-21 22:00:17 -0300

    Campos de almoço só aparecem se o box estiver marcado. fix #2

--- app/assets/javascripts/workdays-new.js
+++ app/assets/javascripts/workdays-new.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $("#workday_lunch").change(function() {
+        if(this.checked){
+            $(".lunch_time").show(500);
+        }else{
+            $(".lunch_time").hide(500);
+        }
+    });
+
 });
